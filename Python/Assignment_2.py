79f55eb46595023276919bd83f14da00b1b3efa4
yathartha04 <yathartha29@gmail.com>
2025-08-14 19:26:16 +0545

    Data Science

---
+++ Python/Assignment_2.py
@@ -0,0 +1,10 @@
+#Task input for every data type we discussed and print it
+
+num = int(input("Enter a whole number: "))
+print(num)
+
+float1 = float(input("Enter a number: "))
+print(float1)
+
+name = input("Enter your email address: ")
+print(name)
---
+++ Python/basicpython.py
@@ -0,0 +1,20 @@
+#print("hello world")
+
+#print("My name is Yathartha Khadgi")
+
+#name = input("Enter your name: ")
+#print(name)
+
+#name = input("Enter your favirote player name: ")
+#print(name)
+
+#num1 = int(input("Enter a number: "))
+#num2 = int(input("Enter another number: "))
+#print(num1 + num2)
+
+print(int("56"))
+print(float(12))
+print(str(56))
+print(int(5.25))
+print(int("Yathartha"))
+
